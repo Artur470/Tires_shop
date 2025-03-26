1f489233e6a62b83765e82ed7747d5b47b5f6f6e
Artur <arturunkulov30@gmail.com>
2025-03-26 16:20:23 +0600

    update code database tiresкупмк

--- config/settings.py
+++ config/settings.py
@@ -191,7 +191,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CORS_ALLOWED_ORIGINS = [
 
-    "https://tires-shop-test.onrender.com",  # Если фронтенд развернут
+    "https://tires-shop-test-jnpy.onrender.com",  # Если фронтенд развернут
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
